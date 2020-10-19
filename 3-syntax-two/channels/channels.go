4aae1a4f67691c07480541e86d408d828e16fe64
Дмитриий Титов <dtsp@yandex.ru>
2020-10-19 18:23:02 +0300

    Примеры для Занятия 3 реорганизованы

---
+++ 3-syntax-two/channels/channels.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	const bufSize = 10
+
+	var bufchan chan int = make(chan int, bufSize) // буферизированный канал
+	bufchan <- 10                                  // неблокирующая операция
+	val := <-bufchan                               // получение значения в новую переменную
+	fmt.Println(val)
+
+	for i := 1; i < bufSize; i++ {
+		bufchan <- i * 2
+	}
+	close(bufchan)
+
+	for val := range bufchan {
+		fmt.Println(val)
+	}
+
+	var ch chan int // переменная типа канал, принимающий int
+	ch = make(chan int)
+	go func(ch chan<- int) { // канал только на отправку
+		ch <- 100
+	}(ch)
+	val = <-ch
+	fmt.Println(val)
+}
+
+// OUTPUT: 100
