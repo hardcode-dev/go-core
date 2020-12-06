1bcce29b1107b991f676a724d9f02e59589e993e
Дмитрий Титов <dtsp@yandex.ru>
2020-12-06 04:23:09 +0300

    Примеры для занятия 17

--- 17-api-2/pkg/api/api.go
+++ 17-api-2/pkg/api/api.go
@@ -1,70 +1,38 @@
 package api
 
 import (
-	"context"
-	"encoding/json"
-	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
 // API предоставляет интерфейс программного взаимодействия.
 type API struct {
 	router *mux.Router
+	store  *sessions.CookieStore
 }
 
 // New создаёт объект API.
 func New(r *mux.Router) *API {
-	api := API{router: r}
+
+	api := API{
+		router: r,
+		store:  sessions.NewCookieStore([]byte("secret_password")),
+	}
 	return &api
 }
 
 // Endpoints регистрирует конечные точки API.
 func (api *API) Endpoints() {
+	api.router.Use(requestIDMiddleware)
 	api.router.Use(logMiddleware)
+	api.router.Use(api.jwtMiddleware)
+	//api.router.Use(api.sessionsMiddleware)
+
+	api.router.HandleFunc("/api/v1/authSession", api.authSession).Methods(http.MethodPost, http.MethodOptions)
+	api.router.HandleFunc("/api/v1/authJWT", api.authJWT).Methods(http.MethodPost, http.MethodOptions)
+
 	api.router.HandleFunc("/api/v1/books", api.books).Methods(http.MethodGet, http.MethodOptions)
 	api.router.HandleFunc("/api/v1/newBook", api.newBook).Methods(http.MethodPost, http.MethodOptions)
-}
-
-func (api *API) books(w http.ResponseWriter, r *http.Request) {
-	err := json.NewEncoder(w).Encode(books)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-}
-
-func (api *API) newBook(w http.ResponseWriter, r *http.Request) {
-	// контекст с таймаутом
-	timeout, cancel := context.WithTimeout(r.Context(), time.Second*10)
-	// освобождение ресурсов, если функция завершится раньше
-	defer cancel()
-	// производный контекст с ключом (№ запроса)
-	ctx := context.WithValue(timeout, "requestID", rand.Intn(1_000_000_000))
-	// длительная перация, принимающая контекст
-	//data := getLotsOfDataFromDatabase(ctx)
-	_ = ctx
-
-	var b book
-	err := json.NewDecoder(r.Body).Decode(&b)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	books = append(books, b)
-}
-
-type book struct {
-	Name   string
-	Author string
-}
-
-var books = []book{
-	{
-		Name:   "The Lord Of The Rings",
-		Author: "J.R.R. Tolkien",
-	},
 }
--- 17-api-2/pkg/api/go.mod
+++ 17-api-2/pkg/api/go.mod
@@ -2,4 +2,8 @@ module go-core/17-api-2/pkg/api
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
+)
